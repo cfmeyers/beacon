2f9a5cde31a72fff3ffdfb9b4da65f7576e347d4
Collin Meyers <cmeyers@renttherunway.com>
2020-07-12 23:18:00 -0400

    Add toggle lights functionality

--- beacon/beacon.py
+++ beacon/beacon.py
@@ -1,3 +1,16 @@
 # -*- coding: utf-8 -*-
+from phue import Bridge
 
 """Main module."""
+
+
+def toggle_lights(bridge_address: str):
+    """toggle_lights.
+
+    Args:
+        bridge_address (str): bridge_address, e.g. "192.168.1.182"
+    """
+    b = Bridge(bridge_address)
+    group = b.get_group(1)
+    is_on = group["action"]["on"]
+    b.set_group(1, "on", not is_on)
--- beacon/cli.py
+++ beacon/cli.py
@@ -3,16 +3,27 @@
 """Console script for beacon."""
 import sys
 import click
+from phue import Bridge
+from beacon.beacon import toggle_lights
 
 
-@click.command()
+@click.group()
 def main(args=None):
-    """Console script for beacon."""
-    click.echo("Replace this message by putting your code into "
-               "beacon.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    """Console script for beacon.py"""
     return 0
 
 
+# TODO https://macintoshguy.wordpress.com/2013/08/26/hacking-the-philips-hue/
+# get color profiles
+
+
+@main.command()
+def toggle():
+    """
+    hue.py toggle
+    """
+    toggle_lights("192.168.1.182")
+
+
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    main()
